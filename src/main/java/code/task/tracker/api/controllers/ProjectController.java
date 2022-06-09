d7006a26abb52e9b5f9239cb085119ee64d0f7ff
ASM717 <stepan151@gmail.com>
2022-06-09 20:25:48 +0300

    ProjectController

---
+++ src/main/java/code/task/tracker/api/controllers/ProjectController.java
@@ -0,0 +1,37 @@
+package code.task.tracker.api.controllers;
+
+import code.task.tracker.api.dto.ProjectDto;
+import code.task.tracker.api.factories.ProjectDtoFactory;
+import code.task.tracker.store.entities.ProjectEntity;
+import code.task.tracker.store.repositories.ProjectRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Transactional
+// аннотация на уровне класса
+@RestController
+// аннотация говорит, что все методы будут возвращать response body
+public class ProjectController {
+
+    ProjectRepository projectRepository;
+    ProjectDtoFactory projectDtoFactory;
+
+    // надо почитать как правильно именовать эндпоинсты
+    public static final String CREATE_PROJECT = "/api/projects";
+
+    @PostMapping(CREATE_PROJECT)
+    public ProjectDto createProject(@RequestParam String name) {
+
+//        ProjectEntity project = projectRepository.
+        return null;
+        //TODO uncommit and insert entity
+        //return projectDtoFactory.makeProjectDto();
+    }
+
+}
--- src/main/java/code/task/tracker/store/repositories/ProjectRepository.java
+++ src/main/java/code/task/tracker/store/repositories/ProjectRepository.java
@@ -3,6 +3,8 @@ package code.task.tracker.store.repositories;
 import code.task.tracker.store.entities.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+import java.util.Optional;
 
+public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+//    Optional<ProjectEntity>
 }
